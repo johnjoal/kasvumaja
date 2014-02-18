773f2e03df1c897a9d26d879c3189673a843cf37
OpenShift guest <aleksei.shupenja@gmail.com>
2014-02-18 05:22:41 -0500

    laravel added

---
+++ laravel/app/controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+class ProductController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+	public function index()
+	{
+		return View::make('products/index');
+	}
+    
+    public function create()
+	{
+		return View::make('products/create');
+	}
+}
